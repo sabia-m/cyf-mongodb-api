ef286280baa4512b61ba5e0b556cd8a8f255909a
Sabia <sabiamushtaq2000@gmail.com>
2020-07-28 23:06:29 +0100

    classwork

--- server.js
+++ server.js
@@ -1,25 +1,64 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const mongodb = require('mongodb');
-const uri = 'mongodb+srv://<username>:<password>@<domain>';
+const uri = process.env.DATABASE_URI
 const mongoOptions = { useUnifiedTopology: true };
 const client = new mongodb.MongoClient(uri, mongoOptions);
 
 const app = express();
 app.use(express.json());
+dotenv.config()
 
 client.connect(function () {
-  const db = client.db('cinema');
+  const db = client.db('mongo-week3');
+  const collection = db.collection("movies")
 
   app.get('/films', function (request, response) {
-    response.send('Retrieve all the films');
+    collection.find().toArray(function(error, films){
+      if (error) {
+        response.status(500).send(error)
+      } else {
+        response.status(200).send(films)
+      }
+    })
   });
 
   app.get('/films/:id', function (request, response) {
-    response.send('Retrieve one film');
+    let id;
+    let checkID = request.params.id
+
+    if (mongodb.ObjectId.isValid(checkID)) {
+      id = new mongodb.ObjectId(checkID)
+    } else {
+      response.status(400).send("This ID is invalid")
+    }
+
+    const searchId = {_id: id}
+
+    collection.findOne(searchId, function(error, result) {
+      if (error){
+        response.status(500).send("Error")
+      } else if(!result){
+        response.status(404).send("Movie not found!")
+      } else {
+        response.send(result)
+      }
+    })
   });
 
   app.post('/films', function (request, response) {
-    response.send('Create a film');
+    const newObject = {
+      title: request.body.title,
+      year: Number(request.body.year)
+    }
+
+    collection.insertOne(newObject, function(error, result){
+      if (error){
+        response.status(500)
+      } else {
+        response.send(result.ops[0])
+      }
+    })
   });
 
   app.put('/films/:id', function (request, response) {
